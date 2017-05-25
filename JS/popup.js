6bace3ca05ab0c7b1e7361aca5931a4e98f44788
8 Win <8 Win>
2017-05-25 20:38:58 +0300

    add slider

--- JS/popup.js
+++ JS/popup.js
@@ -1,10 +1,10 @@
-// fancybox
-$(function() {
-  $('.opinion__hover_block_more ').fancybox({
-    type: 'inline',
-    maxWidth: 460,
-    fitToView: false,
-    padding: 0
+// // fancybox
+// $(function() {
+//   $('.opinion__hover_block_more ').fancybox({
+//     type: 'inline',
+//     maxWidth: 460,
+//     fitToView: false,
+//     padding: 0
 
-  });
-});
+//   });
+// });
