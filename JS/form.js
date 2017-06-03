734d4661f2ca6584b6586b2e04b240ad4ff973c5
Vitalii27 <v.khlystun01@gmail.com>
2017-06-03 09:41:11 +0300

    finished the project

--- JS/form.js
+++ JS/form.js
@@ -6,31 +6,43 @@ $(function() {
             formData = form.serialize();
 
         $.ajax({
-            url: '../php/mail.php',
+            url: '/mail.php',
             type: 'POST',
             data: formData,
             success: function(data) {
-                if (data.status) {
-                    $.magnificPopup.open({
-                        items: {
-                            src: '#success'
-                        },
-                        type: 'inline',
+                var popup = data.status ? '#success' : '#mistake';
 
-                        fixedContentPos: false,
-                        fixedBgPos: true,
+                $.magnificPopup.open({
+                    items: {
+                        src: popup
+                    },
+                    type: 'inline',
+                    fitToView: false,
+                    fixedContentPos: false,
+                    fixedBgPos: true,
 
-                        overflowY: 'auto',
+                    overflowY: 'auto',
 
-                        closeBtnInside: true,
-                        preloader: false,
+                    closeBtnInside: false,
+                    preloader: false,
 
-                        midClick: true,
-                        removalDelay: 300,
-                        mainClass: 'my-mfp-zoom-in'
-                    });
-                }
+                    midClick: true,
+                    removalDelay: 300,
+                    mainClass: 'my-mfp-zoom-in',
+                    callbacks: {
+                        close: function() {
+                            form.trigger('reset');
+                        }
+                    }
+                });
+
             }
         })
     })
+
+    $('.modal__form_close').on('click', function(e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+    })
+
 })
